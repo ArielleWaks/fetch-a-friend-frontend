ac118ad32639336355e43011ad4e7a4e1bb2e494
Jonathan <byersj920@gmail.com>
2024-04-01 16:09:06 -0500

    Submit button created

--- src/components/BrowseJobs.js
+++ src/components/BrowseJobs.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Container, Typography, Autocomplete, TextField} from '@mui/material';
+import { Container, Typography, Autocomplete, TextField, Checkbox, Button} from '@mui/material';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import JobCard from "./JobCard";
 
 import EventBus from "../common/EventBus";
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 const API_URL = "http://localhost:3000/api";
 
@@ -43,7 +48,10 @@ export default function BrowseJobs () {
   const descriptions = [];
 
   for (let i = 0; i < jobArray.length; i++){
-    descriptions.push({label: jobArray[i].description});
+    descriptions.push({label: jobArray[i].description});}
+
+  const handleClick = function(){
+    alert("Clicked!");
   }
   
   return (
@@ -52,8 +60,24 @@ export default function BrowseJobs () {
         Search jobs by...
       </Typography>
 
-      <Autocomplete disablePortal id="combo-box-demo" options={descriptions} sx={{ width: 300 }} 
-      renderInput={(params) => <TextField {...params} label="Animal" />}/>
+      
+    <Autocomplete multiple id="checkboxes-tags-demo" options={descriptions} disableCloseOnSelect
+      getOptionLabel={(option) => option.label} renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.label}
+        </li>
+      )}
+      style={{ width: 500 }}
+      renderInput={(params) => (
+        <TextField {...params} label="Search By Animal(s)" />
+      )}/>
+      <Button variant="contained" onClick={handleClick}>Search</Button>
 
 
       {jobArray.map((field, id) => {
