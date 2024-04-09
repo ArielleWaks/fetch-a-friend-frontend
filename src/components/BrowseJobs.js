7522a4f7d01976fa635135205be1fb16be34560b
Jonathan <byersj920@gmail.com>
2024-04-09 16:02:40 -0500

    Added ability to sort by price (high or low)

--- src/components/BrowseJobs.js
+++ src/components/BrowseJobs.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Container, Typography, Checkbox, Button, Select, MenuItem, FormControl, InputLabel,
 OutlinedInput, ListItemText} from '@mui/material';
 import JobCard from "./JobCard";
-import GetAnimalData from "./functions/jobSearching";
 
 import EventBus from "../common/EventBus";
+
 
 const API_URL = "http://localhost:3000/api";
 
 export default function BrowseJobs () {
   const [jobData, setJobData] = useState([]);
   const [jobArray, setJobArray] = useState([]);
+  const [selectedSortingMethod, setSelectedSortingMethod] = useState([]);
   const [selectedAnimals, setSelectedAnimals] = useState([]);
   
   useEffect(() => {
@@ -46,19 +47,26 @@ export default function BrowseJobs () {
     setJobArray(jobArray.filter((job) => (job.id !== id)));
   } */
 
+ 
   
+  const animalSelections = [];
 
-  const animalSelections = GetAnimalData(jobData);
+  for (let i = 0; i < jobData.length; i++){
+    if ((!animalSelections.includes(jobData[i].chosenAnimalType))){
+    animalSelections.push(jobData[i].chosenAnimalType);}};
 
   const handleAnimalChange = (event) => {
     const { value } = event.target;
     setSelectedAnimals(value);
   };
 
-  const SortByDistanceDescending = function(){
-    //This zip code is a placeholder. The real one will be pulled from the User's profile.
-    let userZipCode = 63074
-  }
+  const handleSortingChange = (event) => {
+    const { value } = event.target;
+    setSelectedSortingMethod(value);
+  };
+
+
+
 
   const processSearch = function(){
     let tempJobArray = [];
@@ -69,8 +77,15 @@ export default function BrowseJobs () {
       const filteredJobs = jobData.filter(job => job.chosenAnimalType === selectedLabel);
       tempJobArray.push(...filteredJobs);}
 
+      if (selectedSortingMethod === 10){
+        tempJobArray.sort((a, b) => a.payRate - b.payRate);
+      } else if (selectedSortingMethod === 20){
+        tempJobArray.sort((a, b) => b.payRate - a.payRate);
+      }
+      
+
     setJobArray(tempJobArray);
-  }
+  };
   
   return (
     <Container>
@@ -79,10 +94,10 @@ export default function BrowseJobs () {
       </Typography>
 
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Animal</InputLabel>
+        <InputLabel id="animal-type-filter">Animal</InputLabel>
         <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
+          labelId="animal-select-checkbox"
+          id="animal-select"
           multiple
           value={selectedAnimals}
           onChange={handleAnimalChange}
@@ -98,6 +113,16 @@ export default function BrowseJobs () {
         </Select>
       </FormControl>
 
+      <FormControl sx={{ m: 1, width: 200 }}>
+      <Select defaultValue={10} id="sorting-method" onChange={handleSortingChange}>
+        <MenuItem value={10}>Pay Rate Hi-Lo</MenuItem>
+        <MenuItem value={20}>Pay Rate Lo-Hi</MenuItem>
+        <MenuItem value={30}>Hours Hi-Lo</MenuItem>
+        <MenuItem value={40}>Hours Lo-Hi</MenuItem>
+      </Select>
+      </FormControl>
+  
+    
       <Button variant="contained" onClick={processSearch}>Search</Button>
 
       {jobArray.map((field, id) => {
